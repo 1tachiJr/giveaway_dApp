6c8a4f812dcb447c58a405307463c427e4d71a07
Nick Allen <nickerdoodle69@gmail.com>
2022-04-05 23:58:11 -0400

    0 price change

--- backend/src/config.js
+++ backend/src/config.js
@@ -53,7 +53,7 @@ const METADATA_UPDATABLE = false; // set to false if you don't want to allow met
 const OWNER_ADDRESS = '0x85438Cb2C5fFD14286f10d25B471A031E05a68cB';
 const TREASURY_ADDRESS = '0x82504eE450f0eBE6c0557cCaEc13F4949BddDD11';
 const MAX_SUPPLY = 100; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 100; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
