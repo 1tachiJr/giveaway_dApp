876e953bb71b3f7c6e4cc67e4e371ab04fd0ef0c
Nick Allen <nickerdoodle69@gmail.com>
2022-04-05 23:52:51 -0400

    real final

--- backend/src/config.js
+++ backend/src/config.js
@@ -68,7 +68,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0xcE6fbFc57239685206c0eAa25c16207692fF87fE"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0xC5CF45FF45A0a0d0964d089443Ed8Ae8bA90487c"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
